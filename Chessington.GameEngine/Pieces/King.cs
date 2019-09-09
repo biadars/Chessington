593b89f151f7e3ba252cd769f17dbc66fa20519f
Bianca Darolti <bianca.darolti@softwire.com>
2019-09-09 11:59:49 +0100

    Add king moves

--- Chessington.GameEngine/Pieces/King.cs
+++ Chessington.GameEngine/Pieces/King.cs
@@ -10,7 +10,18 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            List<Square> moves = new List<Square>();
+            Square position = board.FindPiece(this);
+            moves.Add(Square.At(position.Row - 1, position.Col));
+            moves.Add(Square.At(position.Row - 1, position.Col + 1 ));
+            moves.Add(Square.At(position.Row, position.Col + 1));
+            moves.Add(Square.At(position.Row + 1, position.Col + 1));
+            moves.Add(Square.At(position.Row + 1, position.Col));
+            moves.Add(Square.At(position.Row + 1, position.Col - 1));
+            moves.Add(Square.At(position.Row, position.Col - 1));
+            moves.Add(Square.At(position.Row - 1, position.Col - 1));
+            moves.RemoveAll(move => !Board.InBounds(move));
+            return moves;
         }
     }
 }
